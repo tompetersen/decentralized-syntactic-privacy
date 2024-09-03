f5cf6bf14249b92fcd6bc2c77024efa6362c2e57
Tom Petersen <petersen@informatik.uni-hamburg.de>
2024-09-03 18:50:29 +0200

    minor changes to run.sh

--- eval/run.sh
+++ eval/run.sh
@@ -5,25 +5,23 @@ runs=10
 
 for method in securesum motion ;
 do
-	if [ $method == "securesum" ]; then
-		path="../original"
-	else
-		path="../ours"
-	fi
-	
-	venvpath="../venv/bin/activate"
-	boxpath="$path/run_box.py"
-	centralpath="$path/run_central.py"
+  if [ $method == "securesum" ]; then
+    path="../original"
+  else
+    path="../ours"
+  fi
+  
+  venvpath="../venv/bin/activate"
+  boxpath="$path/run_box.py"
+  centralpath="$path/run_central.py"
 
-	source $venvpath
+  source $venvpath
 
-	for dataset in medical adult ;
-	do
-
-		for ((r=1; r<=$runs; r++))
-		do
-
-      for ((p=2; p<=$max_number_of_parties; p++))
+  for dataset in medical adult ;
+  do
+    for ((p=2; p<=$max_number_of_parties; p++))
+    do
+      for ((r=1; r<=$runs; r++))
       do
         now=$(date +%Y%m%d_%H%M%S)
         logfile="data/runs_$method/$dataset-$p-$now.txt"
@@ -38,9 +36,7 @@ do
 
         echo "[sh] Starting central..."
         python -c "import psutil,sys;network=psutil.net_io_counters(pernic=True);sys.stdout.write('NETWORK BEFORE: ' + str(network) + '\n');" >> $logfile
-
         python $centralpath --number_of_boxes $p --dataset $dataset | tee -a $logfile
-
         python -c "import psutil,sys;network=psutil.net_io_counters(pernic=True);sys.stdout.write('NETWORK AFTER: ' + str(network) + '\n');" >> $logfile
       done
     done
